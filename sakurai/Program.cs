30c950c237ff638c8538e876537bd3a3d8f168f8
thomas <thomasfahey@mail.com>
2021-08-16 15:26:08 +0100

    Add project files.

---
+++ sakurai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sakurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
